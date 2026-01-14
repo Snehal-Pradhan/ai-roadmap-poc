a012586d5687edce5d5ffb348c5edf83fe842bb9
Snehal Pradhan <snehalpradhan23@gmail.com>
2026-01-14 18:27:47 +0530

    added langchain

--- apps/ai-layer/src/index.ts
+++ apps/ai-layer/src/index.ts
@@ -1,1 +1,17 @@
-console.log("HelloWorld");
+import dotenv from 'dotenv';
+dotenv.config()
+
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+
+const apiKey = process.env.GEMINI_API_KEY;
+if (!apiKey) {
+  throw new Error("GEMINI_API_KEY is not defined in environment variables");
+}
+
+const llm = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash",
+  apiKey: apiKey as string,
+  temperature: 0,
+});
+
